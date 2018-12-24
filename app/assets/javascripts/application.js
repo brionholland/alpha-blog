4a8c3eaeb1fda2e4ffdfd91d3f334a6801217a24
Alpha_Blog_Udemy <brionholland2000@gmail.com>
2018-12-24 16:39:19 +0000

    updated to bootstrap 4

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 
-//= require jquery
-//= require jquery_ujs
+//= require jquery3
+//= require popper
 //= require bootstrap-sprockets
 //= require rails-ujs
 //= require activestorage
